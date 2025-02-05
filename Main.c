98a3c27a90d40d5b4deb8387cc4d6021673a6a90
William Souza <souzawpessoal2020@gmail.com>
2025-02-04 23:41:41 -0300

    add qsort function to order array

--- Main.c
+++ Main.c
@@ -24,5 +24,50 @@ typedef struct {
  float preco;
 } PROD; // Estrutura do enunciado
 
-// Teste verificar compatibilidade
+// Q Sort algorithm 
+
+/**************************       
+
+Q Sort algorithm
+
+void qsort(void *base, size_t num, size_t size, int (*compar)(const void *, const void *));
+
+base: Ponteiro para o primeiro elemento do array a ser ordenado.
+
+num: Número total de elementos no array.
+
+size: Tamanho em bytes de cada elemento do array.
+
+compar: Ponteiro para uma função de comparação que define a ordem dos elementos.
+
+Funcionamento:
+
+sort usa o algoritmo QuickSort para reorganizar os elementos do array.
+
+Ele compara pares de elementos usando a função compar fornecida.
+Com base no retorno de compar, qsort decide a posição relativa de dois elementos.
+
+
+**************************/
+
+int compararProdutos(const void *a, const void *b) {
+ const PROD *produtoA = (const PROD *)a;  // Realizando Cast para estrutura do tipo PROD
+ const PROD *produtoB = (const PROD *)b;  // Realizando Cast para estrutura do tipo PROD
+ 
+ return strcmp(produtoA->nome, produtoB->nome); 
+ /*
+ strcmp Compara o nome dos produtos usados:
+  
+ Valor negativo se produtoA->nome vier antes de produtoB->nome (A < B)
+ Zero se forem iguais.
+ Valor positivo se produtoA->nome vier depois de produtoB->nome (A > B).
+ 
+ 
+ */  
+ 
+ 
+ 
+}
+
+
 
