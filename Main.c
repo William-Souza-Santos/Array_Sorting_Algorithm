2389d576e1d577c9b4e562b69af89234a8b7a709
William Souza <souzawpessoal2020@gmail.com>
2025-02-04 23:23:16 -0300

    define struct

--- Main.c
+++ Main.c
@@ -6,6 +6,10 @@
 
                                       WILLIAM SANTOS 
 
+Fazer um programa em "C" que leia uma lista de 20 produtos e preços e armazena-os 
+em um array do tipo da estrutura abaixo. O programa deve, em seguida, ordenar o vetor 
+em ordem ALFABÉTICA de nome de produto.
+
 
 ***************************************************************************************************/
 
@@ -13,4 +17,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+# define Produtos_MAX 20 // declara uma constante com o valor 20
+
+typedef struct {
+ char nome[20];
+ float preco;
+} PROD; // Estrutura do enunciado
 
